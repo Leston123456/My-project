bbdc3dd3284bf62dca553b39efb6f7c07475a3bc
Leston123456 <2115537494@qq.com>
2024-11-03 17:24:13 +0800

    Initial commit

---
+++ Assets/脚本/T2.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class T2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void LoadMyScene()
+    {
+        SceneManager.LoadScene(2);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/脚本/T6.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class T6 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadMyScene()
+    {
+        SceneManager.LoadScene(6);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
